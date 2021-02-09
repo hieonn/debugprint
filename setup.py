460524a6b8558dbc61ee45760e95ab146715893c
MARCEL <hieonn@gmail.com>
2021-02-09 13:00:56 +0900

    version up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
         name    = 'dprint', 
-        version = '1.0.0', 
+        version = '1.1.0', 
         py_modules = ['dprint'], 
         author = 'hieonn', 
         author_email = '',
