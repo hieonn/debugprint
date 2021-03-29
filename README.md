fb096ae971b08efba75f40556ee569acc8d37374
MARCEL <hieonn@gmail.com>
2021-03-30 00:17:02 +0900

    class

--- README.md
+++ README.md
@@ -4,8 +4,12 @@ how to install :
 
 python -m pip install git+https://github.com/hieonn/debugprint.git#egg=dprint
 
+python -m pip install --upgrade git+https://github.com/hieonn/debugprint.git#egg=dprint
+
 how to check :
 
 python -m pip show 
 
 from dprint import dprint, dprintPretty
+dprint_init(True/False)
+dprint(variable, display, tag)
--- dprint.py
+++ dprint.py
@@ -23,67 +23,71 @@ try:
 except ImportError:
     pass
 
-display = False
+class dprint :
 
-def dprint_init(_init) :
-    display = _init
+    self.display = False
 
-def dfprint(df, head=10, embed=True):
-    if embed == True :
-        display(HTML(df.to_html()))
-    else :
-        css = """<style>
-        table { border-collapse: collapse; border: 3px solid #eee; }
-        table tr th:first-child { background-color: #eeeeee; color: #333; font-weight: bold }
-        table thead th { background-color: #eee; color: #000; }
-        tr, th, td { border: 1px solid #ccc; border-width: 1px 0 0 1px; border-collapse: collapse;
-        padding: 3px; font-family: monospace; font-size: 10px }</style>
-        """
-        s  = '<script type="text/Javascript">'
-        s += 'var win = window.open("", "Title", "toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=780, height=400, top="+(screen.height-400)+", left="+(screen.width-840));'
-        s += 'win.document.body.innerHTML = \'' + (df.to_html() + css).replace("\n",'\\') + '\';'
-        s += '</script>'
-        print(df.head(head))
-        return(HTML(s+css))
+    def __init__(self, display) :
+        self.display = display
 
+    def init(self, display) :
+        self.display = display
 
-class PrettyPrinter(pprint.PrettyPrinter):
-	def format(self, _object, context, maxlevels, level):
-		# if isinstance(_object, unicode):
-		# 	return "'%s'" % _object.encode('utf8'), True, False
-		# if isinstance(_object, str):
-		# 	#_object = unicode(_object,'utf8')
-		# 	return "'%s'" % _object.encode('utf8'), True, False
-		return pprint.PrettyPrinter.format(self, _object, context, maxlevels, level)
-
-def dprint(x, _display = True,  tag='unknown'):
-    _display = display and _display
-    if _display == False :
-            return
-    else :
-        print ("-----------------------------------------------------------------------------------")
-        caller = inspect.getframeinfo(inspect.stack()[1][0])
-        if tag == 'unknown' :
-            tag = "line : %s " %(caller.lineno)
+    def dfprint(self, df, head=10, embed=True):
+        if embed == True :
+            display(HTML(df.to_html()))
         else :
-            tag = "line : %s / %s - " %(caller.lineno, tag)
-        frame = inspect.currentframe().f_back
-        s = inspect.getframeinfo(frame).code_context[0]
-        r = re.search(r"\((.*)\)", s).group(1).replace("display=True", "").replace(",", "")
-        r = re.sub(r"tag=.+", "", r)
-        #d = ("{} : {} = {}".format(tag, r,x))
-        data_type = type(x)
-        #if data_type is list or data_type is dict or data_type is tuple or data_type is set or data_type is pandas.core.series.Series :
-        if not (data_type is int or data_type is float or data_type is str or data_type is tuple or data_type is list or data_type is dict) :
-            try :
-                d = ("{} : {} -> {} {}".format(tag, r, type(x), x.shape))
-            except :
-                d = ("{} : {} -> {}".format(tag, r, type(x)))
-            print (d)
-            PrettyPrinter().pprint(x)
+            css = """<style>
+            table { border-collapse: collapse; border: 3px solid #eee; }
+            table tr th:first-child { background-color: #eeeeee; color: #333; font-weight: bold }
+            table thead th { background-color: #eee; color: #000; }
+            tr, th, td { border: 1px solid #ccc; border-width: 1px 0 0 1px; border-collapse: collapse;
+            padding: 3px; font-family: monospace; font-size: 10px }</style>
+            """
+            s  = '<script type="text/Javascript">'
+            s += 'var win = window.open("", "Title", "toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=780, height=400, top="+(screen.height-400)+", left="+(screen.width-840));'
+            s += 'win.document.body.innerHTML = \'' + (df.to_html() + css).replace("\n",'\\') + '\';'
+            s += '</script>'
+            print(df.head(head))
+            return(HTML(s+css))
+
+    class PrettyPrinter(pprint.PrettyPrinter):
+        def format(self, _object, context, maxlevels, level):
+            # if isinstance(_object, unicode):
+            # 	return "'%s'" % _object.encode('utf8'), True, False
+            # if isinstance(_object, str):
+            # 	#_object = unicode(_object,'utf8')
+            # 	return "'%s'" % _object.encode('utf8'), True, False
+            return pprint.PrettyPrinter.format(self, _object, context, maxlevels, level)
+
+    def dprint(self, x, _display = True,  tag='unknown'):
+        _display = self.display and _display
+        if _display == False :
+                return
         else :
-            d = ("{} : {} = {} -> {}".format(tag, r, x, type(x)))
-            print (d)
+            print ("-----------------------------------------------------------------------------------")
+            caller = inspect.getframeinfo(inspect.stack()[1][0])
+            if tag == 'unknown' :
+                tag = "line : %s " %(caller.lineno)
+            else :
+                tag = "line : %s / %s - " %(caller.lineno, tag)
+            frame = inspect.currentframe().f_back
+            s = inspect.getframeinfo(frame).code_context[0]
+            r = re.search(r"\((.*)\)", s).group(1).replace("display=True", "").replace(",", "")
+            r = re.sub(r"tag=.+", "", r)
+            #d = ("{} : {} = {}".format(tag, r,x))
+            data_type = type(x)
+            #if data_type is list or data_type is dict or data_type is tuple or data_type is set or data_type is pandas.core.series.Series :
+            if not (data_type is int or data_type is float or data_type is str or data_type is tuple or data_type is list or data_type is dict) :
+                try :
+                    d = ("{} : {} -> {} {}".format(tag, r, type(x), x.shape))
+                except :
+                    d = ("{} : {} -> {}".format(tag, r, type(x)))
+                print (d)
+                PrettyPrinter().pprint(x)
+            else :
+                d = ("{} : {} = {} -> {}".format(tag, r, x, type(x)))
+                print (d)
 
 # def dprintPretty(x) :
 #     PrettyPrinter().pprint(x)
