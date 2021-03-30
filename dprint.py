2e60c5d932445e56f5be290ddf2de0a05dbad0e7
MARCEL <hieonn@gmail.com>
2021-03-30 17:39:58 +0900

    dprint print

--- dprint.py
+++ dprint.py
@@ -23,37 +23,37 @@ try:
 except ImportError:
     pass
 
-# def dprint(x, display=True, tag='unknown'):
+def dprint(x, display=True, tag='unknown'):
 
-#     if display == False :
-#         return
+    if display == False :
+        return
 
-#         print ("-----------------------------------------------------------------------------------")
-#         caller = inspect.getframeinfo(inspect.stack()[1][0])
-#         if tag == 'unknown' :
-#             tag = "line : %s " %(caller.lineno)
-#         else :
-#             tag = "line : %s / %s - " %(caller.lineno, tag)
-#         frame = inspect.currentframe().f_back
-#         s = inspect.getframeinfo(frame).code_context[0]
-#         r = re.search(r"\((.*)\)", s).group(1).replace("display=True", "").replace(",", "")
-#         r = re.sub(r"tag=.+", "", r)
-#         #d = ("{} : {} = {}".format(tag, r,x))
-#         data_type = type(x)
-#         #if data_type is list or data_type is dict or data_type is tuple or data_type is set or data_type is pandas.core.series.Series :
-#         if not (data_type is int or data_type is float or data_type is str or data_type is tuple or data_type is list or data_type is dict) :
-#             try :
-#                 d = ("{} : {} -> {} {}".format(tag, r, type(x), x.shape))
-#             except :
-#                 d = ("{} : {} -> {}".format(tag, r, type(x)))
-#             print (d)
-#             PrettyPrinter().pprint(x)
-#         else :
-#             d = ("{} : {} = {} -> {}".format(tag, r, x, type(x)))
-#             print (d)
+        print ("-----------------------------------------------------------------------------------")
+        caller = inspect.getframeinfo(inspect.stack()[1][0])
+        if tag == 'unknown' :
+            tag = "line : %s " %(caller.lineno)
+        else :
+            tag = "line : %s / %s - " %(caller.lineno, tag)
+        frame = inspect.currentframe().f_back
+        s = inspect.getframeinfo(frame).code_context[0]
+        r = re.search(r"\((.*)\)", s).group(1).replace("display=True", "").replace(",", "")
+        r = re.sub(r"tag=.+", "", r)
+        #d = ("{} : {} = {}".format(tag, r,x))
+        data_type = type(x)
+        #if data_type is list or data_type is dict or data_type is tuple or data_type is set or data_type is pandas.core.series.Series :
+        if not (data_type is int or data_type is float or data_type is str or data_type is tuple or data_type is list or data_type is dict) :
+            try :
+                d = ("{} : {} -> {} {}".format(tag, r, type(x), x.shape))
+            except :
+                d = ("{} : {} -> {}".format(tag, r, type(x)))
+            print (d)
+            PrettyPrinter().pprint(x)
+        else :
+            d = ("{} : {} = {} -> {}".format(tag, r, x, type(x)))
+            print (d)
 
 
-class dprint() :
+class cprint() :
 
     display = False
 
