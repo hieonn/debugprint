f18d525c33893b0114f3afe004a824b1a9e260f3
MARCEL <hieonn@gmail.com>
2021-03-30 17:25:06 +0900

    dprint print

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
         name    = 'dprint', 
-        version = '1.2.6', 
+        version = '1.2.7', 
         py_modules = ['dprint'], 
         author = 'hieonn', 
         author_email = '',
